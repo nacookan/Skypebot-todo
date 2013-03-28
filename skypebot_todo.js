23d51403be6717f5f0e5927be0208689f4c4271a
nacookan <nacookan@gmail.com>
2013-03-28 12:47:35 +0900

    グループ化の追加、日数指定の追加、ログ出力の追加、投稿内容を先頭1行から30文字に変更。

--- skypebot_todo.js
+++ skypebot_todo.js
@@ -1,6 +1,7 @@
 var todo_path = 'C:\\Users\\username\\Dropbox\\skypebot-todo';
 var todo_file = 'todo.txt';
-var todo_regex = /^(?:todo|Ç∆Ç«|ÉgÉh)[: Å@ÅB]/i;
+var todo_regex = /^([0-9ÇO-ÇX]*)([ Å@]?)(todo|Ç∆Ç«|ÉgÉh)([: Å@ÅB]+)(.+?)([ Å@])/i;
+var countdown_time = '00:00';
 var post_times = ['09:00', '13:00', '17:00'];
 
 // framework
@@ -8,6 +9,10 @@ String.prototype.repeat = function(length){
   var ret = [];
   for(var i = 0; i < length; i++){ ret.push(this); }
   return ret.join('');
+}
+String.prototype.to_narrow_num = function(){
+  return this.replace(/ÇO/g, '0').replace(/ÇP/g, '1').replace(/ÇQ/g, '2').replace(/ÇR/g, '3').replace(/ÇS/g, '4')
+             .replace(/ÇT/g, '5').replace(/ÇU/g, '6').replace(/ÇV/g, '7').replace(/ÇW/g, '8').replace(/ÇX/g, '9');
 }
 Number.prototype.pad0 = function(length){
   return ('0'.repeat(length) + this.toString()).slice(-1 * length);
@@ -41,6 +46,9 @@ while(true){
   if(0 <= post_times.indexOf(hhmm)){
     post(skype);
   }
+  if(countdown_time == hhmm){
+    countdown();
+  }
 }
 
 function receive(msg){
@@ -54,6 +62,7 @@ function receive(msg){
     stream.WriteLine('');
     stream.Close();
     chat.SendMessage('OK.');
+    log('received.');
   }
 }
 
@@ -70,17 +79,67 @@ function post(skype){
       text = stream.ReadAll();
     }
     stream.Close();
-    var lines = text.split(/\r|\n|\r\n/);
+    var lines = text.replace(/\r\n/g, '\n').replace(/\r/g, '\n').split('\n');
+    var map = {};
+    var keys = [];
+    for(var i = 0; i < lines.length; i++){
+      var line = lines[i];
+      if(todo_regex.test(line)){
+        if(RegExp.$1 == ''){
+          if(!map[RegExp.$5]){
+            map[RegExp.$5] = [];
+            keys.push(RegExp.$5);
+          }
+          map[RegExp.$5].push(line.substr(0, 30));
+        }
+      }
+    }
+    if(keys.length != 0){
+      for(var i = 0; i < keys.length; i++){
+        chat.SendMessage(map[keys[i]].join('\n'));
+        log('posted.');
+      }
+    }
+  }
+}
+
+function countdown(){
+  var chats = new Enumerator(skype.Chats);
+  for(; !chats.atEnd(); chats.moveNext()){
+    var chat = chats.item();
+    var fso = new ActiveXObject('Scripting.FileSystemObject');
+    var fullpath = make_fullpath(chat);
+    if(!fso.FileExists(fullpath)) continue;
+    var stream = fso.OpenTextFile(fullpath, 1, false, -2);
+    var text = '';
+    if(!stream.AtEndOfStream){
+      text = stream.ReadAll();
+    }
+    stream.Close();
+    var lines = text.replace(/\r\n/g, '\n').replace(/\r/g, '\n').split('\n');
     var output = [];
     for(var i = 0; i < lines.length; i++){
       var line = lines[i];
       if(todo_regex.test(line)){
-        output.push(line);
+        line = line.replace(todo_regex, function(string, g1, g2, g3, g4, g5, g6){
+          if(g1 != ''){
+            num = parseInt(g1.to_narrow_num(), 10);
+            num--;
+            if(0 < num){
+              g1 = num.toString();
+            }else{
+              g1 = '';
+            }
+          }
+          return g1 + g2 + g3 + g4 + g5 + g6;
+        });
       }
+      output.push(line);
     }
-    if(output.length != 0){
-      chat.SendMessage(output.join('\n'));
-    }
+    var stream = fso.CreateTextFile(fullpath, true, false);
+    stream.WriteLine(output.join('\r\n'));
+    stream.Close();
+    log('countdown.');
   }
 }
 
@@ -88,4 +147,17 @@ function make_fullpath(chat){
   var fso = new ActiveXObject('Scripting.FileSystemObject');
   var name = chat.FriendlyName.replace(/[\\\/\:\*\?\"<>\|]/g, '_');
   return fso.BuildPath(todo_path, name + '_' + todo_file);
+}
+
+function log(message){
+  var d = new Date();
+  var line =
+    d.getFullYear().toString() + '-' +
+    (d.getMonth() + 1).toString() + '-' +
+    d.getDate().toString() + ' ' +
+    d.getHours().toString() + ':' +
+    d.getMinutes().toString() + ':' +
+    d.getSeconds().toString() + ' ' +
+    message;
+  WScript.echo(line);
 }
